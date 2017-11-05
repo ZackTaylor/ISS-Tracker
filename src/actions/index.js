a6db67d87bcc919b81cc25efeb9f734b11051400
Zack Tylor <taylor.zackory@gmail.com>
2017-11-05 16:27:53 -0600

    add toggle lock action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
 export const UPDATE_ISS_POSITION = 'UPDATE_ISS_POSITION';
+export const TOGGLE_LOCK = 'TOGGLE_LOCK';
 
 export const updateIssPosition = () => {
   const request = axios.get('https://api.wheretheiss.at/v1/satellites/25544?units=miles');
   return {
     type: UPDATE_ISS_POSITION,
     payload: request
+  };
+};
+
+export const toggleLock = () => {
+  return {
+    type: TOGGLE_LOCK
   };
 };
