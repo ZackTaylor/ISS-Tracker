279e4be7362e2316f6ee4e82e5665faee8087198
Zack Tylor <taylor.zackory@gmail.com>
2017-11-04 23:18:29 -0500

    demote space station into funcional component

--- src/components/SpaceStation.js
+++ src/components/SpaceStation.js
@@ -1,20 +1,11 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import shouldPureComponentUpdate from 'react-pure-render/function';
+import React from 'react';
 
 import './SpaceStation.css';
 
-export default class MyGreatPlace extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  };
-  static defaultProps = {};
-  shouldComponentUpdate = shouldPureComponentUpdate;
-  render() {
-    return (
-      <div className="iss">
-        <img src={require('../images/iss.png')} alt="iss" />
-      </div>
-    );
-  }
-}
+const SpaceStation = () => (
+  <div className="iss">
+    <img src={require('../images/iss.png')} alt="iss" />
+  </div>
+);
+
+export default SpaceStation;
