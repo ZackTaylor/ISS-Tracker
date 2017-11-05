72bcd81a8b8840bf3fc388f7b67162c21984b2a3
Zack Tylor <taylor.zackory@gmail.com>
2017-11-05 10:52:40 -0600

    get the center prop on the first api call only so the map no longer moves with every iss update.

--- src/reducers/IssReducer.js
+++ src/reducers/IssReducer.js
@@ -1,9 +1,10 @@
-import { GET_ISS_POSITION } from '../actions';
+import { UPDATE_ISS_POSITION } from '../actions';
 
 export default function(state = { coords: { lat: 0, lng: 0 } }, action) {
   switch (action.type) {
-    case GET_ISS_POSITION:
+    case UPDATE_ISS_POSITION:
       return {
+        center: state.center || [Number(action.payload.data.latitude), Number(action.payload.data.longitude)],
         coords: {
           lat: Number(action.payload.data.latitude),
           lng: Number(action.payload.data.longitude)
