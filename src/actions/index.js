b60b1b9b02850d54c578ac09ce3258ac393be635
Zack Tylor <taylor.zackory@gmail.com>
2017-11-05 10:07:40 -0600

    recieve other iss info in miles instead of kilometers

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const GET_ISS_POSITION = 'GET_ISS_POSITION';
 
 export const updateIssPosition = () => {
-  const request = axios.get('https://api.wheretheiss.at/v1/satellites/25544');
+  const request = axios.get('https://api.wheretheiss.at/v1/satellites/25544?units=miles');
   return {
     type: GET_ISS_POSITION,
     payload: request
