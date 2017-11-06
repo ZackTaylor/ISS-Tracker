057b98c41afb75e7a5c965b0e4c1f8b04046a130
Zack Tylor <taylor.zackory@gmail.com>
2017-11-05 19:00:06 -0600

    create the toggle lock action and update the center only when the lockOn prop is true

--- src/reducers/IssReducer.js
+++ src/reducers/IssReducer.js
@@ -1,14 +1,23 @@
-import { UPDATE_ISS_POSITION } from '../actions';
+import { UPDATE_ISS_POSITION, TOGGLE_LOCK } from '../actions';
 
-export default function(state = { coords: { lat: 0, lng: 0 } }, action) {
+export default function(state = { lockOn: true, coords: { lat: 0, lng: 0 }, center: [0, 0] }, action) {
   switch (action.type) {
     case UPDATE_ISS_POSITION:
       return {
-        center: state.center || [Number(action.payload.data.latitude), Number(action.payload.data.longitude)],
+        ...state,
+        center: state.lockOn
+          ? [Number(action.payload.data.latitude), Number(action.payload.data.longitude)]
+          : state.center,
         coords: {
           lat: Number(action.payload.data.latitude),
           lng: Number(action.payload.data.longitude)
         }
+      };
+
+    case TOGGLE_LOCK:
+      return {
+        ...state,
+        lockOn: !state.lockOn
       };
 
     default:
